5c489cc67adb3ee4b7953c2c6ed5ce7fedbd3eaf
cameronjump <cameron.jump@okstate.edu>
2018-10-20 14:16:35 -0500

    Merge branch 'dev' of https://github.com/PoisonPhang/monocle-server into dev

--- src/main/java/com/mhl/monocle/server/MonocleServer.java
+++ src/main/java/com/mhl/monocle/server/MonocleServer.java
@@ -8,27 +8,26 @@ import org.java_websocket.server.WebSocketServer;
 
 public class MonocleServer {
 
-  public static void main( String[] args ) throws InterruptedException , IOException {
-    //Teacher Frontend Web Socket
-    int port = 1337;
-    try {
-      port = Integer.parseInt( args[ 0 ] );
-    } catch ( Exception ex ) {
-    }
-    TeacherServer s = new TeacherServer( port );
-    s.start();
-    System.out.println( "TeacherServer started on port: " + s.getPort() );
+      public static void main (String[]args ) throws InterruptedException, IOException {
+          int port = 1337; // 843 flash policy port
+          try {
+              port = Integer.parseInt(args[0]);
+          } catch (Exception ex) {
+          }
+          TeacherServer s = new TeacherServer(port);
+          s.start();
+          System.out.println("TeacherServer started on port: " + s.getPort());
 
-    //Android Socket
-    ServerSocket serverSocket = new ServerSocket(1336);
-    System.out.println( "AndroidServer started on port: 1336");
-    System.out.println(serverSocket.getInetAddress().getHostAddress());
+          //Android Socket
+          ServerSocket serverSocket = new ServerSocket(1336);
+          System.out.println("AndroidServer started on port: 1336");
+          System.out.println(serverSocket.getInetAddress().getHostAddress());
 
-    while ( true ) {
-      Socket sock = serverSocket.accept();
-      System.out.println("Connected");
-      new Thread(new com.mhl.Android.server.AndroidServer(sock)).start();
+          while (true) {
+              Socket sock = serverSocket.accept();
+              System.out.println("Connected");
+              new Thread(new com.mhl.Android.server.AndroidServer(sock)).start();
 
-    }
+          }
+      }
   }
-}
---
+++ src/main/java/com/mhl/monocle/server/data/DataParse.java
@@ -0,0 +1,12 @@
+package com.mhl.monocle.server.data;
+
+import com.google.gson.Gson;
+import com.mhl.monocle.server.json.DataObject;
+
+public class DataParse {
+//  public static String parseJson(String json) {
+//    Gson gson = new Gson();
+//    DataObject dataObject = gson.fromJson(json, DataObject.class);
+//
+//  }
+}

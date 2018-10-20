9700f23836256daf035ef797df6d5baa0f809734
cameronjump <cameron.jump@okstate.edu>
2018-10-20 12:48:31 -0500

    websocket

--- src/main/java/com/mhl/monocle/server/MonocleServer.java
+++ src/main/java/com/mhl/monocle/server/MonocleServer.java
@@ -1,62 +1,86 @@
-package com.mhl.monocle.server;
-
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.net.ServerSocket;
-import java.net.Socket;
+import java.io.InputStreamReader;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
 
-public class MonocleServer implements Runnable {
-  Socket clientSocket;
+import org.java_websocket.WebSocket;
+import org.java_websocket.WebSocketImpl;
+import org.java_websocket.framing.Framedata;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
 
-  MonocleServer(Socket clientSocket) {
-    this.clientSocket = clientSocket;
+/**
+ * A simple WebSocketServer implementation. Keeps track of a "chatroom".
+ */
+public class MonocleServer extends WebSocketServer {
+
+  public MonocleServer( int port ) throws UnknownHostException {
+    super( new InetSocketAddress( port ) );
   }
 
-  public static void main(String args[]) throws Exception {
-    ServerSocket serverSocket = new ServerSocket(1337);
-    System.out.println("Listening");
-    System.out.println(serverSocket.getInetAddress().getHostAddress());
-
-    while (true) {
-      Socket sock = serverSocket.accept();
-      System.out.println("Connected");
-      new Thread(new MonocleServer(sock)).start();
-    }
+  public MonocleServer( InetSocketAddress address ) {
+    super( address );
   }
 
-  public void run() {
+  @Override
+  public void onOpen( WebSocket conn, ClientHandshake handshake ) {
+    conn.send("Welcome to the server!"); //This method sends a message to the new client
+    broadcast( "new connection: " + handshake.getResourceDescriptor() ); //This method sends a message to all clients connected
+    System.out.println( conn.getRemoteSocketAddress().getAddress().getHostAddress() + " entered the room!" );
+  }
+
+  @Override
+  public void onClose( WebSocket conn, int code, String reason, boolean remote ) {
+    broadcast( conn + " has left the room!" );
+    System.out.println( conn + " has left the room!" );
+  }
+
+  @Override
+  public void onMessage( WebSocket conn, String message ) {
+    broadcast( message );
+    System.out.println( conn + ": " + message );
+  }
+  @Override
+  public void onMessage( WebSocket conn, ByteBuffer message ) {
+    broadcast( message.array() );
+    System.out.println( conn + ": " + message );
+  }
+
+
+  public static void main( String[] args ) throws InterruptedException , IOException {
+    int port = 1337; // 843 flash policy port
     try {
-      InputStream is = clientSocket.getInputStream();
-      PrintWriter pw = new PrintWriter(clientSocket.getOutputStream());
-      pw.println(1);
-      pw.println();
-      pw.flush();
-      byte[] buffer = new byte[1024];
-      int read;
-      while((read = is.read(buffer)) != -1) {
-        String output = new String(buffer, 0, read);
-        System.out.print(output);
-        System.out.flush();
-      }
-      clientSocket.close();
-    } catch (IOException e) {
-      System.out.println(e);
+      port = Integer.parseInt( args[ 0 ] );
+    } catch ( Exception ex ) {
     }
+    MonocleServer s = new MonocleServer( port );
+    s.start();
+    System.out.println( "ChatServer started on port: " + s.getPort() );
 
-//    try {
-//      InputStream inputStream = clientSocket.getInputStream();
-//      PrintWriter printWriter = new PrintWriter(clientSocket.getOutputStream());
-//
-//      byte[] buffer = new byte[1024];
-//      int read;
-//      while ((read = inputStream.read(buffer)) != -1) {
-//        String output = new String(buffer, 0, read);
-//
-//      }
-//
-//    } catch (IOException e) {
-//      e.printStackTrace();
-//    }
+    BufferedReader sysin = new BufferedReader( new InputStreamReader( System.in ) );
+    while ( true ) {
+      String in = sysin.readLine();
+      s.broadcast( in );
+      if( in.equals( "exit" ) ) {
+        s.stop(1000);
+        break;
+      }
+    }
+  }
+  @Override
+  public void onError( WebSocket conn, Exception ex ) {
+    ex.printStackTrace();
+    if( conn != null ) {
+      // some errors like port binding failed may not be assignable to a specific websocket
+    }
+  }
+
+  @Override
+  public void onStart() {
+    System.out.println("Server started!");
+    setConnectionLostTimeout(0);
+    setConnectionLostTimeout(100);
   }
 }
--- src/main/java/com/mhl/monocle/server/data/DataParse.java
+++ src/main/java/com/mhl/monocle/server/data/DataParse.java
@@ -4,9 +4,9 @@ import com.google.gson.Gson;
 import com.mhl.monocle.server.json.DataObject;
 
 public class DataParse {
-  public static String parseJson(String json) {
-    Gson gson = new Gson();
-    DataObject dataObject = gson.fromJson(json, DataObject.class);
-
-  }
+//  public static String parseJson(String json) {
+//    Gson gson = new Gson();
+//    DataObject dataObject = gson.fromJson(json, DataObject.class);
+//
+//  }
 }
